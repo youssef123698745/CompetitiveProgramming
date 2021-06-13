7ffca68873d58aab3914e5877f759255fb354caf
youssef boumhaout <53472982+youssef123698745@users.noreply.github.com>
2021-06-13 23:07:22 +0200

    Create FLOW004.py

---
+++ Codechef/Beginner/FLOW004.py
@@ -0,0 +1,9 @@
+/**
+ *    author:  youssef_boumhaout
+ *    Platform : Codechef.com
+**/
+t = int(input())
+for loop in range(t):
+    n = int(input())
+    n = str(n)
+    print(int(n[0])+int(n[-1]))
