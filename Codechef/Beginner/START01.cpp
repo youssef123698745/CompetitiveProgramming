0294a25aabf2d8b72736002148db5d04495bb691
youssef boumhaout <53472982+youssef123698745@users.noreply.github.com>
2021-06-13 22:36:30 +0200

    Create START01.cpp

---
+++ Codechef/Beginner/START01.cpp
@@ -0,0 +1,18 @@
+/**
+ *    author:  youssef_boumhaout
+ *    Platform : Codechef.com
+**/
+
+#include <iostream>
+using namespace std;
+
+void solve(){
+    int n;
+    cin >> n;
+    cout << n;
+}
+
+int main() {
+	solve();
+	return 0;
+}
