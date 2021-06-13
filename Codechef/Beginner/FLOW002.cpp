2e6e3996061b75c5c8900c2ea2acd1f71c3b996f
youssef boumhaout <53472982+youssef123698745@users.noreply.github.com>
2021-06-13 22:54:45 +0200

    Create FLOW002.cpp

---
+++ Codechef/Beginner/FLOW002.cpp
@@ -0,0 +1,23 @@
+/**
+ *    author:  youssef_boumhaout
+ *    Platform : Codechef.com
+**/
+
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+void solve(){
+    long long t;
+	cin >> t;
+	for(int i=0; i<t; ++i){
+		int a,b;
+		cin >> a >> b ;
+		cout << a%b << "\n";
+	}	
+}
+
+int main(){
+    solve();
+	return 0;
+}
